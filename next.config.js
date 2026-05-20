08ffd447e2152bee3a843d124e82e1531ef770e6
crl1005 <carlosesguerra001@gmail.com>
2026-05-20 15:12:31 +0800

    redeployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   headers: async () => [
     {
       source: "/sw.js",
@@ -12,4 +11,4 @@ const nextConfig: NextConfig = {
   ],
 };
 
-export default nextConfig;
+module.exports = nextConfig;
